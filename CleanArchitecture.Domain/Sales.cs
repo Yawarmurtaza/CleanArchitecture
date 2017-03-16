8b20b7bbdb100f454eb1dcb03a6045312820000d
Yawar Murtaza <yawarmurtaza@gmail.com>
2017-03-16 22:11:31 +0000

    First Commit with only 2 projects both .net core

---
+++ CleanArchitecture.Domain/Sales.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CleanArchitecture.Domain
+{
+    public class Sales : IEntity {
+
+        private int quantity;
+
+        public int Id { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Customer Customer { get; set; }
+        public Employee Employee { get; set; }
+        public Product Product { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; private set; }
+        public int Quantity { get { return this.quantity; } set {
+                this.quantity = value;
+                this.UpdateTotalPrice();
+            } }
+
+        public void UpdateTotalPrice() {
+            this.TotalPrice = this.UnitPrice * this.quantity;
+        }
+    }
+}
